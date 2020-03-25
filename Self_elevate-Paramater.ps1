c2e1202d37e439455005b967f45ad876d3b7bcfe
Heroic-Brine <stefanotoft101112@live.dk>
2020-03-25 17:14:32 +0100

    Merge branch 'master' of https://github.com/Heroic-Brine/powershell-Self-Elevating-Paramater
    Fixed som comments and aded readme

--- Self_elevate-Paramater.ps1
+++ Self_elevate-Paramater.ps1
@@ -1,12 +1,15 @@
-﻿#find ud af om man er administrator lige nu
-Param (
+﻿Param (
+#setting paramater to be requried so my script dosen't break.
  [parameter(Mandatory=$true)]
  [string]
     $PathToScript
  )
+ #checks if the script is allready running as administrator 
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
-#starer dette script som administrator, kan modificeres til at køre et andet script som administartor
+#Starts the specifed script as administartor promting the user to accept a uac dialog
 Start-Process powershell.exe $PathToScript -Verb runAs
 exit
 }
+#If the script is allready administrator there is no need to ask user again for administrator rights
+Start-Process powershell.exe $PathToScript
