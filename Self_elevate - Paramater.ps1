a54e95a7b0a31c05c8962f6694dfb9204091773b
Heroic-Brine <stefanotoft101112@live.dk>
2020-03-25 16:39:29 +0100

    Create Self_elevate - Paramater.ps1

---
+++ Self_elevate - Paramater.ps1
@@ -0,0 +1,15 @@
+﻿#find ud af om man er administrator lige nu
+Param (
+ [parameter(Mandatory=$true)]
+ [string]
+    $PathToScript
+ )
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+#starer dette script som administrator, kan modificeres til at køre et andet script som administartor
+Start-Process powershell.exe $PathToScript -Verb runAs
+exit
+}
+
+Write-Output "test"
+start-sleep 15
